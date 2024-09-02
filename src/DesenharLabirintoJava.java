e412da7320f2dff8ee10a49a9c607c28339f77b2
Jadson Anderson <jadson.anderson.ferreira@gmail.com>
2024-09-02 16:15:15 -0300

    Adiconando alguns arquivos

--- src/DesenharLabirintoJava.java
+++ src/DesenharLabirintoJava.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DesenharLabirinto {
+public class DesenharLabirintoJava {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
--- src/DesenharLabirintoUsandoTecnica.java
+++
@@ -1,9 +0,0 @@
-public class DesenharLabirintoUsandoTecnica {
-
-    public static void main(String[] args) {
-        
-        // Fazer o código implementando a técnica.
-
-    }
-
-}
